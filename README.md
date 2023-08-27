47018c86b1018811f7160085bf9855e4ceece5d1
AROR4 <raghavarora9899@gmail.com>
2023-08-27 09:53:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# wildlife_surveillance
+ Surveillance of wildlife areas without human intervention using microcontroller and operating through app
---
+++ android/app/src/main/kotlin/com/example/wildlife_surveillance/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wildlife_surveillance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
